24dcd2e7bf911a5df8070d55c9700469b6443726
2019211001001217-LiaoWenLong <79953390+Liao-wen-Long@users.noreply.github.com>
2021-06-13 23:27:20 +0800

    HW -commit-WK-15!
    HW -commit-WK-15!

--- src/main/java/com/liaowenlong/controller/AdminOrderListServlet.java
+++ src/main/java/com/liaowenlong/controller/AdminOrderListServlet.java
@@ -1,5 +1,10 @@
 package com.liaowenlong.controller;
 
+
+import com.liaowenlong.dao.OrderDao;
+import com.liaowenlong.model.Order;
+import com.liaowenlong.model.Payment;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,19 +12,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
+import java.util.List;
 
-@WebServlet(name = "UserListServlet", value = "/admin/userList")
-public class UserListServlet extends HttpServlet {
+@WebServlet("/admin/orderList")
+public class AdminOrderListServlet extends HttpServlet {
     Connection con=null;
     public void init() throws ServletException {
         con=(Connection)getServletContext().getAttribute("con");
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String path="/WEB-INF/views/admin/userList.jsp";
+        List<Payment> paymentTypeList=Payment.findAllPayment(con);
+        request.setAttribute("paymentTypeList",paymentTypeList);
+        OrderDao orderDao=new OrderDao();
+        List<Order> orderList=orderDao.findAll(con);
+        request.setAttribute("orderList",orderList);
+        String path="/WEB-INF/views/admin/orderList.jsp";
         request.getRequestDispatcher(path).forward(request,response);
     }
 }
